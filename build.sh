6673b7846f1ca2c39be3406676776bbc81d8615c
overader <aderjasmirzeasrocha@gmail.com>
2023-11-28 17:01:15 -0600

    ACTUALISANDO

--- build.sh
+++ build.sh
@@ -9,4 +9,3 @@ python manage.py migrate
 
 python manage.py collectstatic --no-input
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('overader', 'aderjasmirzeasrocha@gmail.com', '00040')" | python manage.py shell
