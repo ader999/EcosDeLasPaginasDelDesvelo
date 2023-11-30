411c6946ceef270f746876e01d2fd71974b56e41
overader <aderjasmirzeasrocha@gmail.com>
2023-11-29 19:27:33 -0600

    resolviendo imagenes

--- repositorio/settings.py
+++ repositorio/settings.py
@@ -130,6 +130,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -146,8 +148,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 
 LOGIN_URL = 'iniciar_sesion'
 
