0af7992084e521c1227c189bc5f02bcecaa2c3a3
overaderLinux <aderjasmirzeasrocha@gmail.com>
2024-02-04 10:46:42 -0600

    activando debug

--- repositorio/settings.py
+++ repositorio/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+#DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['192.168.1.106','localhost']
 
