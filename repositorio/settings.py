e4ac0fad733b7700a557735abb0a59a2de119e91
overaderLinux <aderjasmirzeasrocha@gmail.com>
2024-05-14 11:34:22 -0600

    Componiendo error2 en 14/05

--- repositorio/settings.py
+++ repositorio/settings.py
@@ -17,11 +17,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecos-de-las-paginas-del-desvelo.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 
 
